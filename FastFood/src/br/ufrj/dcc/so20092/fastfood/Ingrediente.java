b298b242beee5af850576513e907924d02ce8487
raios.catodicos <raios.catodicos@c70234e2-d5f0-11de-82c3-17aed804ed57>
2009-11-20 22:59:19 +0000

    limpeza no código...

--- FastFood/src/br/ufrj/dcc/so20092/fastfood/Ingrediente.java
+++ FastFood/src/br/ufrj/dcc/so20092/fastfood/Ingrediente.java
@@ -17,30 +17,18 @@ public class Ingrediente {
         this.valor = valor;
     }
 
-    /**
-     * @return the valor
-     */
     public double getValor() {
         return valor;
     }
 
-    /**
-     * @param valor the valor to set
-     */
     public void setValor(double valor) {
         this.valor = valor;
     }
 
-    /**
-     * @return the nome
-     */
     public String getNome() {
         return nome;
     }
 
-    /**
-     * @param nome the nome to set
-     */
     public void setNome(String nome) {
         this.nome = nome;
     }
