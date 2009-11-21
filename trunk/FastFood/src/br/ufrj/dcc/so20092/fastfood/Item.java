5b79423c0257a9b659a2cf5ca08afe876f4c3c89
raios.catodicos <raios.catodicos@c70234e2-d5f0-11de-82c3-17aed804ed57>
2009-11-21 03:59:46 +0000

    limpeza e iniciando classe Item...

---
+++ trunk/FastFood/src/br/ufrj/dcc/so20092/fastfood/Item.java
@@ -0,0 +1,17 @@
+package br.ufrj.dcc.so20092.fastfood;
+
+/**
+ *
+ * @author kaiser
+ */
+public class Item {
+    private double precoUnitario;
+
+    public double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+}
---
+++ trunk/FastFood/test/PedidoTest.java
@@ -0,0 +1,12 @@
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author kaiser
+ */
+public class PedidoTest extends TestCase {
+    public void testConstructor() {
+
+    }
+}
