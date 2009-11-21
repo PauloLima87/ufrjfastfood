2d21031eec113fe2052ed66f76683b83244b6fcf
rengenesio <rengenesio@c70234e2-d5f0-11de-82c3-17aed804ed57>
2009-11-21 02:01:06 +0000

    Incluídas as classes bebida, cardapio e pedido

---
+++ trunk/FastFood/src/br/ufrj/dcc/so20092/fastfood/Cardapio.java
@@ -0,0 +1,9 @@
+package br.ufrj.dcc.so20092.fastfood;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Cardapio {
+	private Set<Sanduiche> sanduiches = new HashSet<Sanduiche>();
+	private Set<Bebida> bebidas = new HashSet<Bebida>();
+}
