58fdb61cc07807ddbd19db81bb6cbce2f42ab960
raios.catodicos <raios.catodicos@c70234e2-d5f0-11de-82c3-17aed804ed57>
2009-11-21 13:02:38 +0000

    adicionando TODO's...

--- trunk/FastFood/src/br/ufrj/dcc/so20092/fastfood/Cardapio.java
+++ trunk/FastFood/src/br/ufrj/dcc/so20092/fastfood/Cardapio.java
@@ -1,6 +1,7 @@
 package br.ufrj.dcc.so20092.fastfood;
 
 import br.ufrj.dcc.so20092.fastfood.produto.Bebida;
+import br.ufrj.dcc.so20092.fastfood.produto.Ingrediente;
 import br.ufrj.dcc.so20092.fastfood.produto.Sanduiche;
 import java.util.HashSet;
 import java.util.Set;
@@ -8,4 +9,10 @@ import java.util.Set;
 public class Cardapio {
 	private Set<Sanduiche> sanduiches = new HashSet<Sanduiche>();
 	private Set<Bebida> bebidas = new HashSet<Bebida>();
+
+        //TODO:
+        //Lista de Ingredientes
+        //Lista de bebidas
+        //Lista de sanduiches
+        //Lista de produtos
 }
