f092f5e2b71cea9352b802c934f472ca2f3283c5
rengenesio <rengenesio@c70234e2-d5f0-11de-82c3-17aed804ed57>
2009-12-06 01:49:51 +0000

    teste refeito

--- FastFood/test/BebidaTest.java
+++ FastFood/test/BebidaTest.java
@@ -1,37 +1,17 @@
 import junit.framework.TestCase;
 
-import br.ufrj.dcc.so20092.fastfood.produto.Bebida;
-import br.ufrj.dcc.so20092.fastfood.produto.Ingrediente;
+import br.ufrj.dcc.so20092.fastfood.model.Bebida;
+import br.ufrj.dcc.so20092.fastfood.model.ItemProduto;
+import br.ufrj.dcc.so20092.fastfood.model.Ingrediente;
+import java.util.HashSet;
 
 
 public class BebidaTest extends TestCase {
-    public void testConstructorNomeValor() {
-        Bebida bebida = new Bebida("coca", 2.10);
+    public void testConstructorNome() {
+        HashSet<ItemProduto> itens = new HashSet<ItemProduto>();
+        itens.add(new ItemProduto(new Ingrediente("coca", 2.0), 1));
+        Bebida bebida = new Bebida("coca", itens);
+        assertEquals(bebida.getIngredientes(), itens);
         assertEquals(bebida.getNome(), "coca");
-        assertEquals(bebida.getValorUnidade(), 2.10);
-    }
-
-    public void testGetSetValor() {
-        Bebida bebida = new Bebida();
-        bebida.setValor(1.5);
-        assertEquals(bebida.getValorUnidade(), 1.5);
-    }
-
-    public void testGetSetNome() {
-        Bebida bebida = new Bebida();
-        bebida.setNome("sprite");
-        assertEquals(bebida.getNome(), "sprite");
-    }
-
-    public void testBebidasDiferentes() {
-        Bebida bebida1 = new Bebida("a", 1);
-        Bebida bebida2 = new Bebida("b", 2);
-        assertEquals(bebida1.equals(new Bebida("b", 2)), false);
-    }
-
-    public void testBebidasIguais() {
-        Bebida bebida1 = new Bebida("coca", 1.2);
-        Bebida bebida2 = new Bebida("coca", 1.2);
-        assertEquals(bebida1.equals(new Bebida("coca", 1.2)), true);
     }
 }
