0c571172fe9ffd36f09d56f684b61b1e1661784e
rengenesio <rengenesio@c70234e2-d5f0-11de-82c3-17aed804ed57>
2009-12-06 05:01:00 +0000

    minor changes...

--- trunk/FastFood/src/br/ufrj/dcc/so20092/fastfood/model/Main.java
+++ trunk/FastFood/src/br/ufrj/dcc/so20092/fastfood/model/Main.java
@@ -1,20 +1,15 @@
 package br.ufrj.dcc.so20092.fastfood.model;
 
-
-
 import br.ufrj.dcc.so20092.fastfood.view.TelaPrincipal;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		TelaPrincipal t = new TelaPrincipal();
-		t.setVisible(true);
-                
-	}
-
-        
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        TelaPrincipal t = new TelaPrincipal();
+        t.setVisible(true);
+    }
 }
