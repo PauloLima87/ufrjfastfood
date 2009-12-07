4e1570263a0c667817ce573cad0c56b3fb8f8124
thiagorudiger <thiagorudiger@c70234e2-d5f0-11de-82c3-17aed804ed57>
2009-12-07 01:22:55 +0000

    estoque + relatorio

--- trunk/FastFood/src/br/ufrj/dcc/so20092/fastfood/model/Estoque.java
+++ trunk/FastFood/src/br/ufrj/dcc/so20092/fastfood/model/Estoque.java
@@ -3,48 +3,48 @@ package br.ufrj.dcc.so20092.fastfood.model;
 import java.util.HashSet;
 
 public class Estoque {
-    private HashSet<ItemEstoque> itemEstoque;
+    public static HashSet<ItemEstoque> itensEstoque;
 
     public Estoque () {
-        itemEstoque = new HashSet<ItemEstoque>();
-        itemEstoque.add(new ItemEstoque(Cardapio.alface, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.azeite, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.beterraba, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.cenoura, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.creamCheese, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.ispraite, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.koka, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.pao, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.paoForma, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.peitoFrango, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.peitoPeru, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.phanta, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.presuntoParma, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.queijoMinas, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.requeijao, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.rucula, 1000));
-        itemEstoque.add(new ItemEstoque(Cardapio.tomate, 1000));
+        itensEstoque = new HashSet<ItemEstoque>();
+        itensEstoque.add(new ItemEstoque(Cardapio.alface, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.azeite, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.beterraba, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.cenoura, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.creamCheese, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.ispraite, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.koka, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.pao, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.paoForma, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.peitoFrango, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.peitoPeru, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.phanta, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.presuntoParma, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.queijoMinas, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.requeijao, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.rucula, 1000));
+        itensEstoque.add(new ItemEstoque(Cardapio.tomate, 1000));
     }
 
     public HashSet<ItemEstoque> getItemEstoque() {
-        return itemEstoque;
+        return itensEstoque;
     }
     public void setItemEstoque(HashSet<ItemEstoque> itemEstoque) {
-        this.itemEstoque = itemEstoque;
+        this.itensEstoque = itemEstoque;
     }
 
     public void adiciona(Ingrediente ingrediente, Integer quantidade) {
-        for (ItemEstoque item : itemEstoque) {
+        for (ItemEstoque item : itensEstoque) {
             if (item.getIngrediente() == ingrediente) {
                 item.setQuantidade(item.getQuantidade() + quantidade);
                 return;
             }
         }
-        this.itemEstoque.add(new ItemEstoque(ingrediente, quantidade));
+        this.itensEstoque.add(new ItemEstoque(ingrediente, quantidade));
     }
 
     public boolean remove(Ingrediente ingrediente, Integer quantidade) {
-        for (ItemEstoque item : itemEstoque) {
+        for (ItemEstoque item : itensEstoque) {
             if (item.getIngrediente() == ingrediente) {
                 if (item.getQuantidade() - quantidade >= 0) {
                     item.setQuantidade(item.getQuantidade() - quantidade);
