25ebb8e2fd9e12dce48cbc1a9504b9f959c5976f
raios.catodicos <raios.catodicos@c70234e2-d5f0-11de-82c3-17aed804ed57>
2009-12-07 08:12:11 +0000

    recriando fila de pagamento

---
+++ FastFood/src/br/ufrj/dcc/so20092/fastfood/model/FilaPagamento.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.ufrj.dcc.so20092.fastfood.model;
+
+/**
+ *
+ * @author kaiser
+ */
+public class FilaPagamento {
+
+}
